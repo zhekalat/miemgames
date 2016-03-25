73097901e46dd9c2f42169e423879ae310db8ce0
ngalayko <ngalayko@gmail.com>
2016-03-25 17:46:02 +0300

    events, players handlers added

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ import tornado.gen as gen
 import tornado_mysql
 import json 
 
-class GameHandler(tornado.web.RequestHandler):
+class GamesHandler(tornado.web.RequestHandler):
 	@gen.coroutine
 	def get(self):
 		conn = yield tornado_mysql.connect(host='127.0.0.1', port=3306, user='ubuntu', passwd='', db='miemgames', charset='utf8')
@@ -18,8 +18,36 @@ class GameHandler(tornado.web.RequestHandler):
 		cur.close()
 		conn.close()
 
+class EventsHandler(tornado.web.RequestHandler):
+	@gen.coroutine
+	def get(self):
+		conn = yield tornado_mysql.connect(host='127.0.0.1', port=3306, user='ubuntu', passwd='', db='miemgames', charset='utf8')
+		cur = conn.cursor()
+		yield cur.execute("SELECT * FROM events")
+		result = {}
+		for i, row in enumerate(cur):
+			result[i] = {'game': str(row[2]), 'title': str(row[3]), 'place': str(row[4])}
+		self.write(str(result))
+		cur.close()
+		conn.close()
+
+class PlayersHandler(tornado.web.RequestHandler):
+	@gen.coroutine
+	def get(self):
+		conn = yield tornado_mysql.connect(host='127.0.0.1', port=3306, user='ubuntu', passwd='', db='miemgames', charset='utf8')
+		cur = conn.cursor()
+		yield cur.execute("SELECT * FROM players")
+		result = {}
+		for i, row in enumerate(cur):
+			result[i] = {'name': str(row[2]), 'num_group': str(row[3]), 'rating': str(row[4])}
+		self.write(str(result))
+		cur.close()
+		conn.close()
+
 application = tornado.web.Application(
-	[(r"/select_games", GameHandler)],
+	[(r"/select_games", GamesHandler)],
+	[(r"/select_events", EventsHandler)],
+	[(r"/select_players", PlayersHandler)],
 	debug = True
 )
 
