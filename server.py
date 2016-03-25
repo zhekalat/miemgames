3f5b83d509c1ff5e5c02a68338b74e6ca33457c4
ngalayko <ngalayko@gmail.com>
2016-03-25 20:46:10 +0300

    255 port now

--- server.py
+++ server.py
@@ -224,6 +224,6 @@ application = tornado.web.Application([
 )
 
 if __name__ == "__main__":
-    http_server = tornado.httpserver.HTTPServer(application)
-    http_server.listen(80)
-    tornado.ioloop.IOLoop.instance().start()
+	http_server = tornado.httpserver.HTTPServer(application)
+	http_server.listen(255)
+	tornado.ioloop.IOLoop.instance().start()
