68e9e2ad8c8ed82d0d3c7d9d604e90fa6de906a6
Ubuntu <ubuntu@ip-172-31-16-38.us-west-2.compute.internal>
2016-03-25 12:07:24 +0000

    add new select

--- server.py
+++ server.py
@@ -1,107 +1,22 @@
 import tornado.httpserver
 import tornado.ioloop
 import tornado.web
+import tornado.gen as gen
 import tornado_mysql
-
-def insert_player(name, group):
-	connection = pymysql.connect(host = 'localhost',
-								 user = 'ubuntu',
-								 password = '',
-								 db = 'miemgames',
-								 charset = 'utf8',
-								 cursorclass = pymysql.cursors.DictCursor)
-	try:
-		with connection.cursor() as cursor:
-			sql = "insert into players (name, group) values (%s, %s);"
-			cursor.execute(sql, (name, group))
-			connection.commit()
-	finally:
-		connection.close()
-
-def insert_event(game, time, place):
-	connection = pymysql.connect(host = 'localhost',
-								 user = 'ubuntu',
-								 password = '',
-								 db = 'miemgames',
-								 charset = 'utf8',
-								 cursorclass = pymysql.cursors.DictCursor)
-	try:
-		with connection.cursor() as cursor:
-			sql = "insert into players (game, time, place) values (%s, %s, %s);"
-			cursor.execute(sql, (game, time, place))
-			connection.commit()
-	finally:
-		connection.close()
-
-def insert_game(name, description, min_players, max_players):
-	connection = pymysql.connect(host = 'localhost',
-								 user = 'ubuntu',
-								 password = '',
-								 db = '',
-								 charset = 'utf8',
-								 cursorclass = pymysql.cursors.DictCursor)
-	try:
-		with connection.cursor() as cursor:
-			sql = "insert into players (name, description, min_players, max_players) values (%s, %s, %d, %d);"
-			cursor.execute(sql, (name, description, min_players, max_players))
-			connection.commit()
-	finally:
-		connection.close()
-
-def insert_participant(event, player):
-	connection = pymysql.connect(host = 'localhost',
-								 user = 'ubuntu',
-								 password = '',
-								 db = 'miemgames',
-								 charset = 'utf8',
-								 cursorclass = pymysql.cursors.DictCursor)
-	try:
-		with connection.cursor() as cursor:
-			sql = "insert into players (event, player) values (%d, %d);"
-			cursor.execute(sql, (event, player))
-			connection.commit()
-	finally:
-		connection.close()
-
-def select_games():
-	result = {} 
-
-	connection = pymysql.connect(host = 'localhost',
-								 user = 'ubuntu',
-								 password = '',
-								 db = 'miemgames',
-								 charset = 'utf8',
-								 cursorclass = pymysql.cursors.DictCursor)
-	try:
-		with connection.cursor() as cursor:
-			sql = "select * from games;"
-			cursor.execute(sql)
-			result = cursor.fetchall()
-	finally:
-		connection.close()
-
-	return result
-
-@gen.coroutine
-def main():
-    conn = yield tornado_mysql.connect(host='127.0.0.1', port=3306, user='ubuntu', passwd='', db='miemgames', charset='utf8')
-    cur = conn.cursor()
-    yield cur.execute("SELECT name FROM games")
-    print(cur.description)
-    for row in cur:
-       print(row)
-    cur.close()
-    conn.close()
+import json 
 
 class GameHandler(tornado.web.RequestHandler):
+	@gen.coroutine
 	def get(self):
-	ioloop.IOLoop.current().run_sync(main)
-		#db = database.Connection("localhost", "miemgames", user = "ubuntu")
-		#for game in d.query("SELECT * FROM games"):
-		#        print(game)
-
-		#for game in select_games():
-		#	self.write(game)
+		conn = yield tornado_mysql.connect(host='127.0.0.1', port=3306, user='ubuntu', passwd='', db='miemgames', charset='utf8')
+		cur = conn.cursor()
+		yield cur.execute("SELECT * FROM games")
+		result = {}
+		for i, row in enumerate(cur):
+			result[i] = {'name': str(row[2]), 'min_players': str(row[3]), 'max_players':  str(row[4]), 'description': str(row[1])}
+		self.write(str(result))
+		cur.close()
+		conn.close()
 
 application = tornado.web.Application(
 	[(r"/select_games", GameHandler)],
