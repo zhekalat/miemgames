49198e0f628c04eefd562826646e55e7fb67bf90
Latyshev Evgeniy <e.latishev@gmail.com>
2016-04-09 16:27:11 +0300

    Исходники Android
    Возможно, чего-то не хватает.

---
+++ TabbedPageDemo.Android/GestureListener.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Android.Views;
+
+namespace HSE_APP.Droid
+{
+	public class GesutreListener : GestureDetector.SimpleOnGestureListener
+	{
+		public override bool OnFling (MotionEvent e1, MotionEvent e2, float velocityX, float velocityY)
+		{
+			return base.OnFling (e1, e2, velocityX, velocityY);
+		}
+	}
+}
