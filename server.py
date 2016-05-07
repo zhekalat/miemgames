d003c22a4f9432bfbe0679f70895caf5df2b6497
ngalayko <ngalayko@gmail.com>
2016-05-07 11:54:54 +0300

    changed port to :80

--- server.py
+++ server.py
@@ -340,5 +340,5 @@ application = tornado.web.Application([
 
 if __name__ == "__main__":
 	http_server = tornado.httpserver.HTTPServer(application)
-	http_server.listen(255)
+	http_server.listen(80)
 	tornado.ioloop.IOLoop.instance().start()
